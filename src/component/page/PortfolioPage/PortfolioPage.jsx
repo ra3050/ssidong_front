a6d6220e298f9de1289c74600c381b59849c1850
ra3050 <hek33kr@gmail.com>
2025-10-11 14:17:01 +0900

    fix: 시동

--- src/component/page/PortfolioPage/PortfolioPage.jsx
+++ src/component/page/PortfolioPage/PortfolioPage.jsx
@@ -1,7 +1,16 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react";
 import PortfolioDetail from "./PortfolioDetail";
-import { Wrapper, Title, PortfolioBox, PfCategoryBox, PfCategory, PfContentBox, PfContent, PfContentImg } from "./ProtfolioHTML"
-import { requestPortfolio } from "../../../request/requestAPI"
+import {
+  Wrapper,
+  Title,
+  PortfolioBox,
+  PfCategoryBox,
+  PfCategory,
+  PfContentBox,
+  PfContent,
+  PfContentImg,
+} from "./ProtfolioHTML";
+import { requestPortfolio } from "../../../request/requestAPI";
 
 var portfolioList;
 var pfFilterList;
@@ -13,96 +22,150 @@ var pfFilterList;
  * @param {*} value 포트폴리오 카테고리
  */
 const makeContentList = (portfolioList, value, handle) => {
-    const concernList = []
-    const currentPFList = []
-    const pfcontentWidth = (window.innerWidth * 0.7 - 180) / 3 - 32;
-    const pfcontentHeight = pfcontentWidth * 9 / 16;
+  const concernList = [];
+  const currentPFList = [];
+  const pfcontentWidth = (window.innerWidth * 0.7 - 180) / 3 - 32;
+  const pfcontentHeight = (pfcontentWidth * 9) / 16;
 
-    if (!portfolioList) return []
+  if (!portfolioList) return [];
 
-    for (let i = 0; i < portfolioList.length; i++) {
-        if (value === '전체' || value === portfolioList[i].category) {
-            const num = concernList.length
-            currentPFList.push(portfolioList[i])
-            concernList.push(
-                <PfContent title={portfolioList[i].title} width={pfcontentWidth} height={pfcontentHeight} onClick={() => handle({ rows: num })} key={i}>
-                    <PfContentImg src={portfolioList[i].thumnailLink} />
-                </PfContent>
-            );
-        }
+  for (let i = 0; i < portfolioList.length; i++) {
+    if (value === "전체" || value === portfolioList[i].category) {
+      const num = concernList.length;
+      currentPFList.push(portfolioList[i]);
+      concernList.push(
+        <PfContent
+          title={portfolioList[i].title}
+          width={pfcontentWidth}
+          height={pfcontentHeight}
+          onClick={() => handle({ rows: num })}
+          key={i}
+        >
+          <PfContentImg src={portfolioList[i].thumnailLink} />
+        </PfContent>
+      );
     }
-    pfFilterList = currentPFList;
-    return concernList;
-}
+  }
+  pfFilterList = currentPFList;
+  return concernList;
+};
 
 const PortfolioPage = () => {
-    const [contentList, setContentList] = useState([]);
-    const [detail, setDetail] = useState(<></>);
-    const categoryRef = [useRef(), useRef(), useRef(), useRef(), useRef()]
-    const pfboxWidth = window.innerWidth * 0.7 - 180;
+  const [contentList, setContentList] = useState([]);
+  const [detail, setDetail] = useState(<></>);
+  const categoryRef = [useRef(), useRef(), useRef(), useRef(), useRef()];
+  const pfboxWidth = window.innerWidth * 0.7 - 180;
 
-    useEffect(() => {
-        requestPortfolio((data) => {
-            if (!data) {
-                portfolioList = [];
-                return;
-            } else {
-                categoryRef[0].current.style.opacity = 1;
-                portfolioList = data;
-                const newContentList = makeContentList(portfolioList, '전체', handleContent);
+  useEffect(() => {
+    requestPortfolio((data) => {
+      if (!data) {
+        portfolioList = [];
+        return;
+      } else {
+        categoryRef[0].current.style.opacity = 1;
+        portfolioList = data;
 
-                setContentList(newContentList);
-            }
-        });
-    }, []);
+        console.log(portfolioList);
 
-    /**
-     * PfCategory handle
-     * 색상 스타일 변경, 콘텐츠 생성
-     */
-    const handlePfCategory = (e) => {
-        for (let i = 0; i < categoryRef.length; i++) {
-            categoryRef[i].current.style.opacity = 0.5;
-            console.log(categoryRef[i].current.value)
-        }
-        e.target.style.opacity = 1;
+        const newContentList = makeContentList(
+          portfolioList.data,
+          "전체",
+          handleContent
+        );
 
-        const chooseCategory = e.target.value;
-        const newContentList = makeContentList(portfolioList, chooseCategory, handleContent);
         setContentList(newContentList);
-    };
+      }
+    });
+  }, []);
 
-    const handleContent = (e) => {
-        const rows = e.rows
-        console.log(rows)
-        setDetail(<PortfolioDetail handleDetailDrop={handleDetailDrop} portfolio={pfFilterList} rows={rows} scrolltoTop={document.documentElement.scrollTop || document.body.scrollTop} />)
+  /**
+   * PfCategory handle
+   * 색상 스타일 변경, 콘텐츠 생성
+   */
+  const handlePfCategory = (e) => {
+    for (let i = 0; i < categoryRef.length; i++) {
+      categoryRef[i].current.style.opacity = 0.5;
+      console.log(categoryRef[i].current.value);
     }
+    e.target.style.opacity = 1;
 
-    const handleDetailDrop = () => {
-        console.log('click')
-        setDetail(<></>);
-    }
+    const chooseCategory = e.target.value;
+    const newContentList = makeContentList(
+      portfolioList.data,
+      chooseCategory,
+      handleContent
+    );
+    setContentList(newContentList);
+  };
 
-    return (
-        <Wrapper>
-            {detail}
+  const handleContent = (e) => {
+    const rows = e.rows;
+    console.log(rows);
+    setDetail(
+      <PortfolioDetail
+        handleDetailDrop={handleDetailDrop}
+        portfolio={pfFilterList}
+        rows={rows}
+        scrolltoTop={
+          document.documentElement.scrollTop || document.body.scrollTop
+        }
+      />
+    );
+  };
 
-            <Title>WORKS</Title>
-            <PortfolioBox>
-                <PfCategoryBox>
-                    <PfCategory ref={categoryRef[0]} onClick={handlePfCategory} value='전체'>{"전체"}</PfCategory>
-                    <PfCategory ref={categoryRef[1]} onClick={handlePfCategory} value='광고&바이럴'>광고&바이럴</PfCategory>
-                    <PfCategory ref={categoryRef[2]} onClick={handlePfCategory} value='유튜브콘텐츠'>유튜브콘텐츠</PfCategory>
-                    <PfCategory ref={categoryRef[3]} onClick={handlePfCategory} value='모션그래픽'>모션그래픽</PfCategory>
-                    <PfCategory ref={categoryRef[4]} onClick={handlePfCategory} value='시동을 걸다'>시동을 걸다</PfCategory>
-                </PfCategoryBox>
+  const handleDetailDrop = () => {
+    console.log("click");
+    setDetail(<></>);
+  };
 
-                <PfContentBox width={pfboxWidth}>
-                    {contentList}
-                </PfContentBox>
-            </PortfolioBox>
-        </Wrapper>
-    )
-}
+  return (
+    <Wrapper>
+      {detail}
 
-export default PortfolioPage
+      <Title>WORKS</Title>
+      <PortfolioBox>
+        <PfCategoryBox>
+          <PfCategory
+            ref={categoryRef[0]}
+            onClick={handlePfCategory}
+            value="전체"
+          >
+            {"전체"}
+          </PfCategory>
+          <PfCategory
+            ref={categoryRef[1]}
+            onClick={handlePfCategory}
+            value="광고&바이럴"
+          >
+            광고&바이럴
+          </PfCategory>
+          <PfCategory
+            ref={categoryRef[2]}
+            onClick={handlePfCategory}
+            value="유튜브콘텐츠"
+          >
+            유튜브콘텐츠
+          </PfCategory>
+          <PfCategory
+            ref={categoryRef[3]}
+            onClick={handlePfCategory}
+            value="모션그래픽"
+          >
+            모션그래픽
+          </PfCategory>
+          <PfCategory
+            ref={categoryRef[4]}
+            onClick={handlePfCategory}
+            value="시동을 걸다"
+          >
+            시동을 걸다
+          </PfCategory>
+        </PfCategoryBox>
+
+        <PfContentBox width={pfboxWidth}>{contentList}</PfContentBox>
+      </PortfolioBox>
+    </Wrapper>
+  );
+};
+
+export default PortfolioPage;
--- src/request/requestAPI.js
+++ src/request/requestAPI.js
@@ -1,35 +1,38 @@
 import axios from "axios";
 
 const AxiosHttpData = {
-    // baseURL: 'http://localhost:3050/',
-    baseURL: 'http://192.168.0.221:3050/',
-    headers: {
-        'Content-Type': 'application/json; charset=UTF-8'
-    },
-    responseType: 'json',
-    responseEncoding: 'json'
-}
+  // baseURL: 'http://localhost:3050/',
+  // baseURL: 'http://192.168.0.221:3050/',
+  baseURL: "http://192.168.219.100:3050/",
+  headers: {
+    "Content-Type": "application/json; charset=UTF-8",
+  },
+  responseType: "json",
+  responseEncoding: "json",
+};
 
-const Axios = axios.create(AxiosHttpData)
+const Axios = axios.create(AxiosHttpData);
 
 export const requestPortfolio = async (callback) => {
-    const value = await Axios.get('/api/requestPortfolio').then((req, error) => {
-        return (req.data);
-    }).catch((error) => {
-        return false;
+  const value = await Axios.get("/api/requestPortfolio")
+    .then((req, error) => {
+      return req.data;
     })
+    .catch((error) => {
+      return false;
+    });
 
-    callback(value);
-}
+  callback(value);
+};
 
 export const requestSendEmail = async (props, callback) => {
-    const value = await Axios.post('/api/sendmailToContact', props).then((req, error) => {
-        return (req.data);
-    }).catch((error) => {
-        return false;
+  const value = await Axios.post("/api/sendmailToContact", props)
+    .then((req, error) => {
+      return req.data;
     })
+    .catch((error) => {
+      return false;
+    });
 
-    callback(value);
-}
-
-
+  callback(value);
+};
